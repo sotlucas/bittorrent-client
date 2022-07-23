2b7a9369a627a9ef60871d43f5e7eafe681dfff5
NContinanza <ncontinanza@fi.uba.ar>
2022-07-23 18:33:21 -0300

    WIP: remove comments from request.rs

--- dtracker/src/tracker_request/request.rs
+++ dtracker/src/tracker_request/request.rs
@@ -1,40 +1,52 @@
-use std::net::TcpStream;
+use std::{net::TcpStream, io::Read};
+
+use crate::http::http_parser::{Http, HttpError};
 
 pub struct Request {
     pub stream: TcpStream,
 }
 
+pub enum RequestError {
+    InvalidEndpointError,
+    ParseHttpError   
+}
+
 impl Request {
     pub fn new(stream: TcpStream) -> Request {
-        Request { stream: stream }
+        Request { stream }
     }
 
-    pub fn handle(&self) {
+    pub fn handle(&self) -> Result<(), RequestError> {
+        let mut buf = vec![];
+        let body = self.stream.read_to_end(&mut buf).unwrap();
 
-        // let body = self.stream.read_to_end().unwrap();
-
-        // let http_request = Http::parse(&body);
-
-        // match http_request.endpoint {
-        //     "announce" => {self.handle_announce(http_request);},
-        //     "stats" => {self.handle_stats(http_request);},
-        //     _ => { Err(()) }
-        // };
+        let http_request = Http::parse(&buf).map_err(|_| RequestError::ParseHttpError)?;
+        // TODO: Validate method in request
+        match http_request.endpoint.as_str() {
+            "/announce" => {
+                self.handle_announce(http_request);
+            }
+            "/stats" => {
+                self.handle_stats(http_request);
+            }
+            _ => return Err(RequestError::InvalidEndpointError),
+        };
+        Ok(())
     }
 
-    // fn handle_announce(&self) {
-    //     let announce_response = AnnounceResponse::from(http_request.params);
-    // }
+    fn handle_announce(&self, http_request: Http) {
+        let announce_response = AnnounceResponse::from(http_request.params);
+    }
 
-    // fn handle_stats(&self) {
-    //     let stats_response = Stats::from(http_request.params);
-    //     self.send_response(response);
-    // }
+    fn handle_stats(&self, http_request: Http) {
+        let stats_response = Stats::from(http_request.params);
+        self.send_response(response);
+    }
 
-    // fn send_response(&self, response: String) {
-    //    stream
-    //    .write_all(create_response(buffer)?.as_bytes())
-    //    .unwrap();
-    //    stream.flush().unwrap();
-    //  }
+    fn send_response(&self, response: String) {
+        stream
+            .write_all(create_response(buffer)?.as_bytes())
+            .unwrap();
+        stream.flush().unwrap();
+    }
 }
